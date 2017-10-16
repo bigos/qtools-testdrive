d14a77210466027f3d4f62dbc603e11c04a6ebac
Jacek Podkanski <ruby.object@googlemail.com>
2017-10-16 08:12:53 +0100

    second example works

--- src/qtools-testdrive.lisp
+++ src/qtools-testdrive.lisp
@@ -6,8 +6,21 @@
 
 (named-readtables:in-readtable :qtools)
 
+(define-widget window (QWidget)
+  ())
+
+(define-subwidget (window text-edit) (q+:make-qtextedit))
+
+(define-subwidget (window quit-button) (q+:make-qpushbutton "&Quit!" window))
+
+(define-subwidget (window layout) (q+:make-qvboxlayout window)
+  (q+:add-widget layout text-edit)
+  (q+:add-widget layout quit-button))
+
+(define-slot (window button-pressed) ()
+  (declare (connected quit-button (clicked)))
+  (format t "quitting")
+  (q+:close window))
+
 (defun main ()
-  (let ((widget (#_new QTextEdit)))
-    (#_setWindowTitle widget "Hello!")
-    (#_show widget)
-    (#_exec *qapplication*)))
+  (with-main-window (window (make-instance 'window))))
