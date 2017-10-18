893e2338129270c00397831a6e9969cecb8136a4
Jacek Podkanski <ruby.object@googlemail.com>
2017-10-18 16:19:33 +0100

    message box works

--- src/qtools-testdrive.lisp
+++ src/qtools-testdrive.lisp
@@ -6,6 +6,16 @@
 
 (named-readtables:in-readtable :qtools)
 
+(defun my-quit ()
+  (let ((message-box (#_new QMessageBox )))
+    (format t "quit function ")
+    (q+::set-window-title message-box "Notepad")
+    (q+::set-text message-box "Are you sure you want to quit?")
+    (q+::set-standard-buttons message-box (logior (q+::qmessagebox.yes) (q+::qmessagebox.no)))
+    (q+::set-default-button message-box (q+::qmessagebox.no))
+    (when (equalp (q+::exec message-box) (q+::qmessagebox.yes))
+      (q+::qcoreapplication-quit))))
+
 (define-widget window (QWidget)
   ())
 
@@ -19,8 +29,11 @@
 
 (define-slot (window button-pressed) ()
   (declare (connected quit-button (clicked)))
-  (format t "quitting")
-  (q+:close window))
+  (my-quit))
+
+
 
 (defun main ()
-  (with-main-window (window (make-instance 'window))))
+  (with-main-window (window (make-instance 'window))
+   (q+:set-window-title window "Notepad")
+                     ))
