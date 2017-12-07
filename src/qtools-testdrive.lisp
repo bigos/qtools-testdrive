ea51e8b6b7ef39474ba4568e822836968a7f2324
Jacek Podkanski <ruby.object@googlemail.com>
2017-12-07 23:54:01 +0000

    code cleanup and readme ferinement

--- src/qtools-testdrive.lisp
+++ src/qtools-testdrive.lisp
@@ -1,3 +1,4 @@
+(declaim (optimize (debug 3) (speed 0)))
 ;;;; qtools-testdrive.lisp
 
 (in-package #:qtools-testdrive)
@@ -8,24 +9,33 @@
 
 
 (defun my-quit ()
-  (let ((message-box (q+::make-qmessagebox)))
-    (q+::set-window-title message-box "Notepad")
-    (q+::set-text message-box "Are you sure you want to quit?")
-    (q+::set-standard-buttons message-box (logior (q+::qmessagebox.yes) (q+::qmessagebox.no)))
-    (q+::set-default-button message-box (q+::qmessagebox.no))
-    (when (equalp (q+::exec message-box) (q+::qmessagebox.yes))
-      (q+::qcoreapplication-quit))))
+  (let ((message-box (q+:make-qmessagebox)))
+    (q+:set-window-title message-box "Notepad")
+    (q+:set-text message-box "Are you sure you want to quit?")
+    (q+:set-standard-buttons message-box (logior (q+:qmessagebox.yes) (q+:qmessagebox.no)))
+    (q+:set-default-button message-box (q+:qmessagebox.no))
+    (when (equalp (q+:exec message-box) (q+:qmessagebox.yes))
+      (q+:qcoreapplication-quit))))
 
 (defun my-open (window)
-  (let ((file-name (q+::qfiledialog-get-open-file-name window "Open your first QT file" "" "*.*" ))
+  (let ((file-name (q+:qfiledialog-get-open-file-name window "Open your first QT file" "" "*.*" ))
         (buffer (make-string 4096))
         (read 0))
     (unless (equalp file-name "")
+      (setf *current-file* file-name)
       (with-open-file (in file-name)
         (format t "file name ~S~%" file-name)
         (setf read (read-sequence buffer in))
         (with-slots-bound (window window)
-          (q+::set-text text-edit (subseq buffer 0 read)))))))
+          (q+:set-text text-edit (subseq buffer 0 read)))))))
+
+(defun my-save (Window)
+  (let ((file-name *current-file*))
+    (with-open-file (fs file-name
+                        :direction :output
+                        :if-exists :supersede)
+      (with-slots-bound (window window)
+        (format fs "~A" (q+:to-plain-text text-edit))))))
 
 (define-widget window (QMainWindow)
   ())
@@ -39,7 +49,7 @@
          (my-open window))
   (:separator)
   (:item ("Save" (ctrl s))
-         (my-quit))
+         (my-save window))
   (:separator)
   (:item ("Quit" (ctrl q))
          (my-quit)))
@@ -49,8 +59,8 @@
   (q+:add-widget layout quit-button)
   ;; set layout as central widget of window
   (let ((appwindow (q+:make-qwidget window)))
-    (setf (q+::layout appwindow) layout)
-    (setf (q+::central-widget window) appwindow)))
+    (setf (q+:layout appwindow) layout)
+    (setf (q+:central-widget window) appwindow)))
 
 (define-slot (window button-pressed) ()
   (declare (connected quit-button (clicked)))
